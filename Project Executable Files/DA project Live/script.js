81ba21d77b1f18d1ab813e6364a3f9932d0a891f
Suryasrirayudu <suryasrirayudu@gmail.com>
2026-02-25 13:25:11 +0530

    Add files via upload

---
+++ Project Executable Files/DA project Live/script.js
@@ -0,0 +1,3 @@
+// This file is currently empty since your dashboards only use Tableau’s embed script.
+// You can add custom JS here if you want to handle events or interactions in future.
+console.log("Dashboards loaded successfully!");
